bf5bb560e36488690611186c39e017c30c04900d
gyarram <gkreddy.yarram@gmail.com>
2020-04-29 20:51:44 -0400

    Local Workspace

---
+++ Training/config/components/integration_server/startup.js
@@ -0,0 +1,1 @@
+global.__iib_require = require;
---
+++ root_odjfs/src/root_odjfs/jsonDto.java
@@ -0,0 +1,49 @@
+package root_odjfs;
+
+public class jsonDto {
+	
+	public String firstName;
+	public String lastName;
+	public String ssn;
+	public String Address;
+	public String getAddress() {
+		return Address;
+	}
+	public void setAddress(String address) {
+		Address = address;
+	}
+	public String getCity() {
+		return City;
+	}
+	public void setCity(String city) {
+		City = city;
+	}
+	public String getState() {
+		return State;
+	}
+	public void setState(String state) {
+		State = state;
+	}
+	public String City;
+	public String State;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getSsn() {
+		return ssn;
+	}
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+	}
+	
+
+}
